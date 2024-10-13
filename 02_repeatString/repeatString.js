18624bdda10984ee1c663318a644551d57473d59
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-13 12:58:54 +1000

    Update repeatString func to pass Jest tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+  let newString = "";
+  if (n >= 0) {
+    for (let i = 0; i < n; i++) {
+      newString += string;
+    }
+    return newString
+  } else if (n < 0) {
+    return "ERROR"
+  }
 };
 
 // Do not edit below this line
