4e1053214c7b8fcbd09244f33d5c40bd76c8bf43
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-13 14:18:51 +1000

    Edit reverseString func to pass Jest tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const chars = [];
+  const reverseChars = [];
 
+  for (let i = 0; i < string.length; i++) {
+    chars[i] = string[i];
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    reverseChars[i] = chars.pop();
+  }
+
+  return reverseChars.join("");
 };
 
 // Do not edit below this line
