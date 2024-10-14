5032a0c34832ae186fa30d4736440b4646f408f7
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-14 20:14:07 +1000

    Update removeFromArray to pass Jest tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...removeArgs) {
+  for (let arg of removeArgs){
+    while (array.includes(arg)) {
+      if (array.includes(arg)) {
+        index = array.indexOf(arg);
+        array.splice(index, 1);
+      }
+    }
+  }
+  return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
