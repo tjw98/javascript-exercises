1e2f9ab89af6b760f50261163aae9d57801a4dc9
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-14 20:56:36 +1000

    Update sumAll func to pass Jest tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  // checks arguments are positive integers
+  if (n1 < 0 || n2 < 0 || !Number.isInteger(n1) || !Number.isInteger(n2)) {
+    return "ERROR";
+  }
 
+  let bigger = n1 > n2 ? n1 : n2;
+  let smaller = n1 > n2 ? n2 : n1; 
+  let sum = smaller;
+
+  for (let i = smaller + 1; i < bigger + 1; i++) {
+    sum+= i;
+  }
+  
+  return sum;
 };
 
 // Do not edit below this line
