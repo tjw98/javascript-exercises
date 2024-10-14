c72f2da2a36af55cc7fe9d7ceee7612e83735251
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-14 21:07:59 +1000

    Update leapYears func to pass Jest tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // returns true if leap year
+  if (year % 4 == 0 && year % 100 != 0 ||year % 400 == 0) return true;
+  return false;
 };
 
 // Do not edit below this line
