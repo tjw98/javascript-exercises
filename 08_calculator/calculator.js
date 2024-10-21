133ee32aa7e4e14fb79923f923238060ce7ef603
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-21 20:38:58 +1000

    Update calculator functions to pass Jest tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,20 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (sumArray) => sumArray.reduce((accumulator, current) => accumulator + current, 0);
 
-const multiply = function() {
+const multiply = (productArray) => productArray.reduce((accumulator, current) => accumulator * current);
 
-};
+const power = (a, b) => a ** b;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = (n) => {
+  let total = 1;
+  for (let i = n; i > 0; i--) {
+    total *= i;
+  }
+  return total;
+}
 
 // Do not edit below this line
 module.exports = {
