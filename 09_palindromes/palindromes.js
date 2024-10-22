9227034bd7cdebec1b9c694447f26cb11fa720fe
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-22 20:51:33 +1000

    Update palindromes funct to pass Jest tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = str => {
+  const ignoredChars = [".", " ", "!", ","]
+  const newStr = str
+  .toLowerCase()
+  .split("")
+  .filter((element) => !ignoredChars.includes(element))
+  .join("");
 
-};
-
+  const reverseStr = newStr.split("").reverse().join("");
+  
+  return newStr === reverseStr
+}
 // Do not edit below this line
 module.exports = palindromes;
