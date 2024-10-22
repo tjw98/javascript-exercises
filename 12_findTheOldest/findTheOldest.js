36e00c0d37ce3fad26e36fbf5468b570f5defbc1
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-22 18:27:22 +1000

    Update findTheOldest funct to pass Jest tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // Get todays year for people still alive
+  let currentYear = new Date().getFullYear();
 
+  // Assigns current year to person if they are alive
+  people.map((person) => {
+    if (!person.hasOwnProperty("yearOfDeath")) {
+      return person.yearOfDeath = currentYear
+    }
+  });
+  
+  people.sort((a, b) => {
+    const aAge = a.yearOfDeath - a.yearOfBirth;
+    const bAge = b.yearOfDeath - b.yearOfBirth;
+
+    return aAge < bAge ? 1 : -1;
+  });
+  return people[0]
 };
 
 // Do not edit below this line
